0f660b9621ae670509a6fc7c64e18be93d808614
Itu <sebastianitu61@yahoo.com>
2023-05-29 17:25:11 +0300

    Resolved YES and NO imgs from subchars and secaccounts

--- src/Components/SecondaryAccount/NoSecondaryAccount.jsx
+++ src/Components/SecondaryAccount/NoSecondaryAccount.jsx
@@ -5,7 +5,7 @@ import './SecondaryAccount.css'
 function NoSecondaryAccount() {
     return (
         <div className='noSecondaryAccount my-2 border-4 border-red-800 w-full mx-auto h-1/3 flex items-center border-2 '>
-            <img class='w-1/6 mx-auto' src="src\assets\no.png" alt="LIPSA CONT" />
+            <img class='w-1/6 mx-auto' src="no.png" alt="LIPSA CONT" />
         </div>
     )
 }
--- src/Components/Subcharacter/NoSubCharacter.jsx
+++ src/Components/Subcharacter/NoSubCharacter.jsx
@@ -5,7 +5,7 @@ import './NoSubCharacter.css'
 function NoSubCharacter() {
     return (
         <div className='noSubCharacter mx-2 my-2 border-4 border-red-800 w-1/5 mx-auto h-full flex items-center border-2'>
-            <img class='w-1/5 mx-auto' src="src\assets\no.png" alt="LIPSA SUBCARACTER" title='LIPSA SUBCARACTER' />
+            <img class='w-1/5 mx-auto' src="no.png" alt="LIPSA SUBCARACTER" title='LIPSA SUBCARACTER' />
         </div>
     )
 }
