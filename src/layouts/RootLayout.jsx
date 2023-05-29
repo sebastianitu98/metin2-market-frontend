08ce0cc1eb33af96914a32cb48f2150fe6f40da2
Itu <sebastianitu61@yahoo.com>
2023-05-29 12:32:58 +0300

    deleted logo text

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,7 +6,7 @@ function RootLayout() {
         <div className='font-sans font-bold text-neutral-950 min-h-screen bg-black'>
             <header>
                 <nav className='navigation flex justify-between mx-auto my-2 px-2 my-0 items-center  text-zinc-300'>
-                    <NavLink to="/"><img className='w-14 mx-auto' src="https://img.m2icondb.com/72346.png" alt="LOGO" />MACHU PICHUU</NavLink>
+                    <NavLink to="/"><img className='w-14 mx-auto' src="https://img.m2icondb.com/72346.png" alt="LOGO" /></NavLink>
                     <NavLink to="/">PLATFORME</NavLink>
                     <NavLink to="contact">CONTACT</NavLink>
                     <NavLink to="about">INFO</NavLink>
