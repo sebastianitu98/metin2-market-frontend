9c120bdb0d0d6b2c2e4fb3ababb8caa234408cd2
Itu <sebastianitu61@yahoo.com>
2023-05-29 22:07:16 +0300

    changed social media links changed Info page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Contact() {
     return (
         <div className='flex flex-col text-white mx-auto text-center h-screen my-20'>
-            <a href="#" className='text-xl my-2'><i class="fa-brands fa-discord"></i> DISCORD - Machu#1061</a>
-            <a href="#" className='text-xl my-2'><i class="fa-brands fa-instagram"></i> INSTAGRAM</a>
-            <a href="#" className='text-xl my-2'><i class="fa-brands fa-facebook"></i> FACEBOOK </a>
-            <a href="#" className='text-xl my-2'><i class="fa-brands fa-whatsapp"></i> WHATSAPP</a>
-            <a href="#" className='text-xl my-2'><i class="fa-brands fa-skype"></i> SKYPE</a>
+            <a href="#" className='text-xl my-2'><i class="fa-brands fa-discord"></i> DISCORD - ITU#9920</a>
+            <a href="https://www.instagram.com/sebastianitu98/" className='text-xl my-2'><i class="fa-brands fa-instagram"></i> INSTAGRAM - sebastianitu98</a>
+            <a href="https://www.facebook.com/sebastian.itu.9" className='text-xl my-2'><i class="fa-brands fa-facebook"></i> FACEBOOK - Sebastian Itu</a>
+            {/* <a href="#" className='text-xl my-2'><i class="fa-brands fa-whatsapp"></i> WHATSAPP</a> */}
+            <a href="https://join.skype.com/invite/jo71cD7sVrjQ" className='text-xl my-2'><i class="fa-brands fa-skype"></i> SKYPE - ITU SEBASTIAN</a>
             {/* <a href="#"><i class="fa-brands fa-youtube"></i>YOUTUBE</a> */}
 
             <p className='text-xl my-8 mx-auto'>MA PUTETI CONTACTA PE RETELELE DE SOCIALIZARE DE MAI SUS</p>
